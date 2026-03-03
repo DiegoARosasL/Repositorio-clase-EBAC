556d1654b687e39c3b81bb8806fc42b1021ae59d
DiegoARosasL <okabekurisusama@gmail.com>
2026-03-03 10:23:11 -0600

    Start Update Loop
    -Se hizo la diferencia entre start y update

--- Proyecto Inicial EBAC/Assets/Scripts/HolaMundo.cs
+++ Proyecto Inicial EBAC/Assets/Scripts/HolaMundo.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+
+    int x;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        x = 0;
         print("Algo paso");
-        Debug.Log("Algo paso");
+        
         Debug.LogWarning("Algo Salio Medianamente Mal");
         Debug.LogError("Algo Salio Muy Mal");
     }
@@ -14,6 +17,7 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        x = x + 1;
+        Debug.Log("x");
     }
 }
