0a7a04ce9715de88052b6839c3b6724e5c4da1b1
DiegoARosasL <okabekurisusama@gmail.com>
2026-03-05 11:07:09 -0600

    Actividad Modulo 6
    2da Parte

---
+++ Proyecto Inicial EBAC/Assets/Módulo 6/Script/CrearCuboOn.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class CrearCuboOn : MonoBehaviour
+{
+    GameObject objToSpawn;
+    Vector3[] vertices = {
+        new Vector3 (0, 0, 0), //vertice0
+        new Vector3 (1, 0, 0), //vectice1
+        new Vector3 (1, 1, 0), //vectice2
+        new Vector3 (0, 1, 0), //vectice3
+        new Vector3 (0, 1, 1), //vectice4
+        new Vector3 (1, 1, 1), //vectice5
+        new Vector3 (1, 0, 1), //vectice6
+        new Vector3 (0, 0, 1), //vectice7
+        };
+    int[] triangulos = {
+            0, 2, 1, //Cara 1
+            0, 3, 2,
+            2, 3, 4, //Cara 2
+            2, 4, 5,
+            1, 2, 5, //Cara 3
+            1, 5, 6,
+            0, 7, 4, //Cara 4
+            0, 4, 3,
+            5, 4, 7, //Cara 5
+            5, 7, 6,
+            0, 6, 7, //Cara 6
+            0, 1, 6
+    };
+    private void OnEnable()
+    {
+        objToSpawn = new GameObject("Nuestro Primer Cubo");
+        objToSpawn.AddComponent<MeshFilter>();
+        var meshFilter = objToSpawn.GetComponent<MeshFilter>().mesh;
+        meshFilter.Clear();
+        meshFilter.vertices = vertices;
+        meshFilter.triangles = triangulos;
+        meshFilter.Optimize();
+        meshFilter.RecalculateNormals();
+        objToSpawn.AddComponent<BoxCollider>();
+        var boxCollider = objToSpawn.GetComponent<BoxCollider>();
+        boxCollider.center = new Vector3(0.5f, 0.5f, 0.5f);
+        objToSpawn.AddComponent<MeshRenderer>();
+        var meshRenderMaterial = objToSpawn.GetComponent<MeshRenderer>().material;
+        meshRenderMaterial.color = Color.white;
+        objToSpawn.transform.position = Vector3.one;
+        Debug.Log("El Cubo esta Activo");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        Debug.Log("El Cubo se Desactivo");
+    }
+}
---
+++ Proyecto Inicial EBAC/Assets/Módulo 6/Script/GenerarCubos.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GenerarCubos : MonoBehaviour
+{
+    public GameObject PrefabCubo;
+    public List<GameObject> listaDeCubos;
+    public float factorDeEscalamiento;
+    public int numCubos = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        listaDeCubos = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        numCubos++;
+        GameObject tempGameObject = Instantiate<GameObject>(PrefabCubo);
+        tempGameObject.name = "CuboNumero" + numCubos;
+        Color c = new Color(Random.value, Random.value, Random.value);
+        tempGameObject.GetComponent<MeshRenderer>().material.color = c;
+        tempGameObject.transform.position = Random.insideUnitSphere;
+
+        listaDeCubos.Add(tempGameObject);
+        List<GameObject> objetosParaEliminar = new List<GameObject>();
+        foreach (GameObject go in listaDeCubos)
+        {
+            float scale = go.transform.localScale.x;
+            scale *= factorDeEscalamiento;
+            go.transform.localScale = Vector3.one * scale;
+
+            if (scale <= 0.1)
+            {
+                objetosParaEliminar.Add(go);
+            }
+        }
+
+        foreach (GameObject go in objetosParaEliminar)
+        {
+            listaDeCubos.Remove(go);
+            Destroy(go);
+        }
+    }
+}
